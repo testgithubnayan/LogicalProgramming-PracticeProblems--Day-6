22f75df4a04b1c78ffb7baa9b367b5d416d51a36
NAYAN SONI <107099130+testgithubnayan@users.noreply.github.com>
2022-11-08 08:16:14 +0530

    Entering the screen detailes and getting day of week.

---
+++ DayOfWeeks.java
@@ -0,0 +1,41 @@
+package com.brigelabz;
+import java.util.Scanner;
+
+public class DayOfWeek {
+	
+
+    static void dayOfWeek(int m, int d, int y) {
+        int y0 = y -(14-m)/12;
+        int x = y0 + y0/4 - y0/100 + y0/400;
+        int m0 = m+12*((14-m)/12)-2;
+        int d0=(d+x+31*m0/12)%7;
+
+        if (d0 == 0) {
+            System.out.println("Sunday");
+        } else if (d0 == 1) {
+            System.out.println("Monday");
+        } else if (d0 == 2) {
+            System.out.println("Tuesday");
+        } else if (d0 == 3) {
+            System.out.println("Wednesday");
+        } else if (d0 == 4) {
+            System.out.println("Thursday");
+        } else if (d0 == 5) {
+            System.out.println("Friday");
+        } else {
+            System.out.println("Saturday");
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter month");
+        int m = sc.nextInt();
+        System.out.println("Enter day");
+        int d = sc.nextInt();
+        System.out.println("Enter year");
+        int y = sc.nextInt();
+        System.out.println();
+        dayOfWeek(m, d, y);
+    }
+}
