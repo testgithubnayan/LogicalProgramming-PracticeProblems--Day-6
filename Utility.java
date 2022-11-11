cb2b8c01b8e796e1fa0dc2254c011cb8cee02f56
Nayan Soni <you@example.com>
2022-11-11 18:07:36 +0530

    new java file

---
+++ Utility.java
@@ -0,0 +1,14 @@
+package javaprogrames;
+
+public class Utility {
+
+	public static int binary(int[] arr, int n) {
+		
+		return 0;
+		StaticTest ref = new StaticTest();
+		System.out.println(StaticTest.z);
+		System.out.println(ref.z);
+		System.out.println(ref.addition());
+	}
+
+}
